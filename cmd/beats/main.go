42243845978bf578458a510f069c2e08e0eb8044
Nicolas Bettenburg <nicbet@gmail.com>
2025-12-18 16:36:01 +0100

    implement config via viper, style issue type in beats ls

--- cmd/beats/main.go
+++ cmd/beats/main.go
@@ -4,13 +4,24 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/palarix/beats/internal/config"
 	"github.com/spf13/cobra"
 )
+
+var cfg *config.Config
 
 var rootCmd = &cobra.Command{
 	Use:   "beats",
 	Short: "A JSONL-based issue tracker",
 	Long:  `Beats is a JSONL-based issue tracker that is committed to Git together with your project.`,
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		var err error
+		cfg, err = config.LoadConfig()
+		if err != nil {
+			return err
+		}
+		return nil
+	},
 }
 
 func main() {
