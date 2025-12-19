431da1219400cbcc0590faf6422e7f18bfc3b75a
Nicolas Bettenburg <nicbet@kuy.io>
2025-12-19 20:38:35 +0100

    autocomplete for history command, better format of email address in history

--- cmd/beats/history.go
+++ cmd/beats/history.go
@@ -11,9 +11,10 @@ import (
 )
 
 var historyCmd = &cobra.Command{
-	Use:   "history [id]",
-	Short: "Show audit trail for an issue",
-	Args:  cobra.ExactArgs(1),
+	Use:               "history [id]",
+	Short:             "Show audit trail for an issue",
+	Args:              cobra.ExactArgs(1),
+	ValidArgsFunction: completeIssueIDs,
 	Run: func(cmd *cobra.Command, args []string) {
 		id := args[0]
 
